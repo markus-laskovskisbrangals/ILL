c19ff510a0de287919bbcd4a3b47e59f3e060dc5
markus-laskovskisbrangals <markus.laskovskisbrangals@va.lv>
2022-06-05 20:07:12 +0300

    Added register style

--- register.php
+++ register.php
@@ -3,16 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="./stylesheets/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reģistrēties</title>
 </head>
 <body>
-<form action="" method="POST">
-        <input type="text" name="username" id="" placeholder="Lietotājvārds">
-        <input type="email" name="email" id="" placeholder="E-pasts">
-        <input type="password" name="password" id="" placeholder="Parole">
-        <input type="password" name="repeat-password" id="" placeholder="Parole atkārtoti">
-        <input type="submit" value="Reģistrēties">
-    </form>
+    <div class="form-container">
+        <h2>Reģistrēties</h2>
+        <form action="" method="POST">
+            <input type="text" name="username" id="" placeholder="Lietotājvārds">
+            <input type="email" name="email" id="" placeholder="E-pasts">
+            <input type="password" name="password" id="" placeholder="Parole">
+            <input type="password" name="repeat-password" id="" placeholder="Parole atkārtoti">
+            <input type="submit" value="Reģistrēties" id="submit-button">
+        </form>
+        <p>Esi jau reģistrējies? <a href="login.php">Ienākt</a></p>
+    </div>
 </body>
 </html>
