f79017026e1207a13bb227daf0ad17fca961c815
markus-laskovskisbrangals <markus.laskovskisbrangals@va.lv>
2022-06-05 19:54:53 +0300

    Added login design

--- login.php
+++ login.php
@@ -3,14 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="./stylesheets/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ienākt</title>
 </head>
 <body>
-    <form action="" method="POST">
-        <input type="text" name="username" id="" placeholder="Lietotājvārds">
-        <input type="password" name="password" id="" placeholder="Parole">
-        <input type="submit" value="Ienākt">
-    </form>
+    <div class="form-container">
+        <h2>Ienākt</h2>
+        <form action="" method="POST">
+            <input type="text" name="username" id="" placeholder="Lietotājvārds">
+            <input type="password" name="password" id="" placeholder="Parole">
+            <input type="submit" value="Ienākt" id="submit-button">
+        </form>
+        <p>Vēl neesi reģistrējies? <a href="register.php">Reģistrēties</a></p>
+    </div>
 </body>
 </html>
