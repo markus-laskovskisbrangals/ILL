56ccc64ddc75b8fe57d825ec12462663284cbbd8
Emīls Daivids Kļaviņš <daiiviids@gmail.com>
2022-06-05 22:40:44 +0300

    Fixed Error #2

--- database.php
+++ database.php
@@ -10,6 +10,7 @@ $connection = 'mysql:host='.$host.';dbname='.$db.';charset=UTF8;';
 try{
     //Jauna PDO objekta izveida
     $pdo = new PDO($connection, $user, $pass);
+    return $pdo;
 
 } catch(PDOException $e){
     echo 'Pieslēgums datubāzei neizdevās: '.$e;
