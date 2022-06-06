358ae9745b1235f95fc4ad039d6009ea1261bfa1
Emīls Daivids Kļaviņš <daiiviids@gmail.com>
2022-06-06 20:20:38 +0300

    foruma noteikumi

---
+++ rules.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="./stylesheets/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Par Outpost From</title>
+</head>
+<?php session_start(); ?>
+<body>
+    <div class="banner"></div>
+        <?php require './components/navigation.php'; ?>
+        <div class="container">
+            <div class="chat-widget" style="display: inline-block; margin: 35px;">
+                <div class="widget-header">
+                    <p>Foruma noteikumi</p>
+                </div>
+                <div class="widget-body">
+<br>Visiem serveriem ir jāievēro un jāievieš Discord "Pakalpojumu sniegšanas noteikumi" (ToS). Par jebkuru personu, kas pārkāpj Discord "ToS" un/vai vadlīnijas, tiks ziņots un tā tiks izslēgta no servera. Pilnīgs vadlīniju un noteikumu sadalījums ir sniegts zemāk: <br><br>
+
+Discord vadlīnijas: https://discordapp.com/guidelines<br>
+Discord pakalpojumu sniegšanas noteikumi: https://discordapp.com/terms<br><br>
+
+Mēs esam angļu serveris.  Lai mēs varētu atbalstīt savus spēlētājus, ievērot noteikumus un aizsargāt ikvienu, visai saziņai (ja vien nav norādīts citādi) jābūt angļu valodā.<br><br>
+
+Saskaņā ar Discord noteikumiem visiem lietotājiem jābūt vismaz 13 gadus veciem.<br><br>
+
+1. Vienmēr cieniet cits citu. <br>
+2. Visas ziņas, valoda, attēli utt. ir jāuzskata par drošiem darbam. <br>
+3. VIENMĒR izlasiet piespraustos ziņojumus. <br>
+4. Nesūtiet surogātpastu.<br>
+5. Serverī netiek pieļauts apzināti likt cilvēkiem justies neērti.<br>
+6. Avatariem, lietotājvārdiem, emotīviem, statusiem un sarunām jābūt drošiem darbam.<br>
+7. Aizliegtās tēmas: Aizliegtas tēmas: Politika, reliģija, pretrunīgi, aizvainojoši un (NSFW) temati. <br>
+8. Naids runas, rasistiski/eksistējoši, rupjības vai nelikumīgas ziņas/komunikācija nozīmē tūlītēju aizliegumu.<br>
+9. Rīkojies kā pieaugušais.<br>
+10. Ziņojiet par jebkādu nevēlamu uzvedību personāla loceklim.<br>
+11. Netroļojiet un nesabojājiet pieredzi citiem. Tas attiecas arī uz balss kanāliem.<br>
+12. Nulles tolerance pret jebkādu nelikumīgu/necilvēcīgu rīcību serverī, balss čatā vai DM.<br>
+13. Par ļaunprātīgu uzvedību var saņemt brīdinājumu un/vai beztermiņa aizliegumu.<br>
+14. Viltus apsūdzības tiks pārbaudītas un attiecīgi izskatītas.<br>
+                </div>
+                <div class="widget-body"></div>
+            </div>
+        </div>
+</body>
+</html>
